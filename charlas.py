e13c2d801ae1de2e923e1528f9c55da27516655f
Nahuel <nahuel.defosse@gmail.com>
2014-10-27 12:55:29 -0300

    Versión mergeable

---
+++ charlas.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <headingcell level=1>
+
+# Hacking academia for fun and profit
+
+# <headingcell level=1>
+
+# La revolución de Jupyter
+
+# <headingcell level=1>
+
+# Robótica Educativa con Android + Python + Pingüino-PIC18F4550
+
+# <headingcell level=1>
+
+# Machine Learning y problemas pequeños
+
+# <headingcell level=1>
+
+# Espiando procesos locales con Python-Ptrace
+
+# <headingcell level=1>
+
+# Fotografía y modelamiento geométrico para impresoras 3D
+
+# <headingcell level=1>
+
+# FOP-ARSAT: Simulando la Red Federal de Fibra Óptica usando Python
+
+# <headingcell level=1>
+
+# Utilizando SVG para visualizar datos estructurados
+
+# <headingcell level=1>
+
+# Desarrollando mecánica de fluidos con Python
+
+# <headingcell level=1>
+
+# Versionado de código con git
+
+# <headingcell level=1>
+
+# Cameras, Apertures, and Contact Lenses
+
+# <headingcell level=1>
+
+# Diez consejos para un mejor software (de) científico(s)
+
+# <headingcell level=1>
+
+# Python Científico: migrando de Matlab a Python 
+
+# <codecell>
+
+Sistema de grabación para detectar hablantes de Buenos Aires y Córdoba
+
+# <codecell>
+
+Diseño de un plan doctoral para el análisis de datos astrofísicos con Python
+
+# <codecell>
+
+Introduction to PyCUDA
+
+# <codecell>
+
+
+# <markdowncell>
+
+# # Charlas Relámpago 22/10
+# 
+# * Visualizando hábitos de músia (Marguglio Nicolás)
+# * Prymatex el proyecto (Diego van Haaster)
+# * Análisis multicriterio (Juan Cabral)
+# * Optimizando código Python: Mini-tips
+# * Freelanscience (for fun, profit & learning) (Pablo Celayes)
+# * Detectando Señas de Truco con Python (Santaigo Avendaño)
+# * Proyección de datos Bipatitios (Rodrigo René Cura)
+# * Pilas en Colegios Secundarios (Rodrigo René Cura, Romina Stickar)
+
+# <codecell>
+
+!ls
+
